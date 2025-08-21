1032455f4a565056477247f471594967e3dfb788
110vatsal <85301381+110vatsal@users.noreply.github.com>
2025-08-22 00:23:57 +0530

    Add files via upload

---
+++ VT_1_1_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+    int num1, num2, sum;
+
+    printf("write two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    sum = num1 + num2;
+    
+    printf("Addition of %d and % d is:%d", num1, num2, sum);
+    
+    return 0;
+}
